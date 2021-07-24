f832e73f96ab2a8fe3a68f05de4655985c9f67f7
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-07-24 09:55:36 +0300

    Project added

---
+++ src/test/java/ru/netology/CallbackTest.java
@@ -0,0 +1,32 @@
+package ru.netology;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class CallbackTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setupClass() {
+        WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+    void setUp() {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldTestSomething() {
+        throw new UnsupportedOperationException();
+    }
+}
