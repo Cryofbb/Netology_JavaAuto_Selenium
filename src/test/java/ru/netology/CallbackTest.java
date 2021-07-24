444ffee2661b34373374cc289ed80a4175ef3cad
Cryofbb <78804095+Cryofbb@users.noreply.github.com>
2021-07-24 11:29:23 +0300

    Tests for "Name" field added and success #1

--- src/test/java/ru/netology/CallbackTest.java
+++ src/test/java/ru/netology/CallbackTest.java
@@ -17,10 +17,55 @@ public class CallbackTest {
     @Test
     void shouldSubmitRequest() {
         SelenideElement form = $("[method=post]");
-        form.$("[data-test-id=name] input").setValue("Василий");
-        form.$("[data-test-id=phone] input").setValue("+79270000000");
+        form.$("[data-test-id=name] input").setValue("Имя");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
         form.$("[data-test-id=agreement]").click();
         form.$("[role=button]").click();
         $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldSubmitRequestWithSpaceInName() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Имя имя");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldSubmitRequestWithDoubleName() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Имя-имя");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=order-success]").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+    }
+    @Test
+    void shouldNotSubmitRequestWithEmptyName() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+    @Test
+    void shouldNotSubmitRequestWithEnglishName() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Name");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+    }
+    @Test
+    void shouldNotSubmitRequestWithSymbolName() {
+        SelenideElement form = $("[method=post]");
+        form.$("[data-test-id=name] input").setValue("Имя%имя");
+        form.$("[data-test-id=phone] input").setValue("+79000000000");
+        form.$("[data-test-id=agreement]").click();
+        form.$("[role=button]").click();
+        $("[data-test-id=name] .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 }
